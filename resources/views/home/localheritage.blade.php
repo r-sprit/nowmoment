d6fa2ba17b5d3b8eb3153f67266276c0946e28f5
shoaib <muhemmed.shoaib@gmail.com>
2017-11-27 23:25:33 +0900

    updated

--- resources/views/home/localheritage.blade.php
+++ resources/views/home/localheritage.blade.php
@@ -101,7 +101,7 @@
 
                 });
             }
-            url = "http://openapi.gg.go.kr/LOCALCULTRELC?key=050f8fa7263748229e91ddf9dfe5f0e5&type=json&pSize=1000";
+            url = "http://openapi.gg.go.kr/LOCALCULTRELC?key=93c8f67b82c9453e92a50dc5d3280ee3&type=json&pSize=1000";
             $.get( url, function( data ) {
                 $( "#results" ).html( data );
                 console.log(data);
@@ -141,7 +141,7 @@
                 center: uluru
             });
 
-            url = "http://openapi.gg.go.kr/LOCALCULTRELC?key=050f8fa7263748229e91ddf9dfe5f0e5&type=json&pSize=1000";
+            url = "http://openapi.gg.go.kr/LOCALCULTRELC?key=93c8f67b82c9453e92a50dc5d3280ee3&type=json&pSize=1000";
 
             $.get( url, function( data ) {
                 $( "#results" ).html( data );
